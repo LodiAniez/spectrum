37d0fe3274dacdbc41f868b99f5c9fb42f36cfd6
Dexter Louie Aniez <dexterlouie.aniez@sun-asterisk.com>
2023-05-19 14:31:01 +0800

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "import", "simple-import-sort"],
+  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/lines-between-class-members": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "lines-between-class-members": "error",
+  },
+}
